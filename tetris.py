ddd5b30e217530e73f287ba8ecb8b75967132610
Robin Vjc <newrotik@gmail.com>
2015-06-07 16:46:54 +1000

    tetramino rotations done

--- tetris.py
+++ tetris.py
@@ -1,3 +1,4 @@
+import numpy as np
 from collections import namedtuple
 
 Position = namedtuple('Position', ['row','column'])
@@ -5,120 +6,137 @@ Block = namedtuple('Block', ['row', 'column', 'color'])
 
 class Tetramino(object):
     "Tetramino object subclasses all the tetraminos"
-    piece_grid_rows = 1
-    piece_grid_cols = 1
-    piece_orientations = 1
+    PIECE_GRID_ROWS = 1
+    PIECE_GRID_COLS = 1
+    PIECE_ORIENTATIONS = 1
 
     def __init__(self, position=Position(0,0)):
         self.position = position
         self.orientation = 0
         # orientations_grid[orientation][row][column]
-        self.orientations_grid = [[['.' for i in range(self.piece_grid_cols)]\
-                                  for j in range(self.piece_grid_rows)]\
-                                  for k in range(self.piece_orientations)]
-        self.piece_grid = self.rotate(self.orientation)
+        self.orientations_grid = np.empty((self.PIECE_ORIENTATIONS,\
+                                           self.PIECE_GRID_ROWS,\
+                                           self.PIECE_GRID_COLS), dtype=object)
+        self.orientations_grid[:,:,:] = '.'
+        self.piece_grid = self.orientations_grid[0,:,:]
 
     def display_piece_grid(self):
-        for row in range(self.piece_grid_rows):
+        for row in range(self.PIECE_GRID_ROWS):
             print ' '.join(self.piece_grid[row])
 
     def test_tetramino(self):
         self.display_piece_grid()
 
-    def rotate(self, orientation):
-        self.piece_grid = self.orientations_grid[orientation][:][:]
-        return self.piece_grid
+    def rotate_right(self):
+        if self.orientation < self.PIECE_ORIENTATIONS-1:
+            self.orientation += 1
+        else:
+            self.orientation = 0
+        self.piece_grid = self.orientations_grid[self.orientation,:,:]
+        # return self.piece_grid
 
 class I_Tetramino(Tetramino):
     """ Implementation of the I Tetramino
     orientation: 0 = horizontal, 1 = vertical """
-    piece_grid_rows = 4
-    piece_grid_cols = 4
-    piece_orientations = 2
+    PIECE_GRID_ROWS = 4
+    PIECE_GRID_COLS = 4
+    PIECE_ORIENTATIONS = 4
 
     def __init__(self, position=Position(0,0)):
         Tetramino.__init__(self, position)
-        # orientation 0
-        self.orientations_grid[0][1][:] = ['c','c','c','c']
-        self.piece_grid = self.rotate(self.orientation)
+        # orientations
+        self.orientations_grid[0,1,:] = ['c','c','c','c']
+        self.orientations_grid[1,:,2] = ['c','c','c','c']
+        self.orientations_grid[2,2,:] = ['c','c','c','c']
+        self.orientations_grid[3,:,1] = ['c','c','c','c']
+        self.piece_grid = self.orientations_grid[0,:,:]
 
 class O_Tetramino(Tetramino):
     """ Implementation of the O Tetramino """
-    piece_grid_rows = 2
-    piece_grid_cols = 2
-    piece_orientations = 1
+    PIECE_GRID_ROWS = 2
+    PIECE_GRID_COLS = 2
+    PIECE_ORIENTATIONS = 1
 
     def __init__(self, position=Position(0,0)):
         Tetramino.__init__(self, position)
-        # orientation 0: 2x2 square 'y' (first row, then second row)
-        self.orientations_grid[0][0][:] = ['y','y']
-        self.orientations_grid[0][1][:] = ['y','y']
-        self.piece_grid = self.rotate(self.orientation)
+        # orientation 0: 2x2 square 'y'
+        self.orientations_grid[0,:,:] = [['y','y'],['y','y']]
+        self.piece_grid = self.orientations_grid[0,:,:]
 
 class Z_Tetramino(Tetramino):
     """ Implementation of the Z Tetramino """
-    piece_grid_rows = 3
-    piece_grid_cols = 3
-    piece_orientations = 2
+    PIECE_GRID_ROWS = 3
+    PIECE_GRID_COLS = 3
+    PIECE_ORIENTATIONS = 4
 
     def __init__(self, position=Position(0,0)):
         Tetramino.__init__(self, position)
-        # orientation 0
-        self.orientations_grid[0][0][:] = ['r','r','.']
-        self.orientations_grid[0][1][:] = ['.','r','r']
-        self.piece_grid = self.rotate(self.orientation)
+        # orientations
+        self.orientations_grid[0,:,:] = [['r','r','.'],['.','r','r'],['.','.','.']]
+        self.orientations_grid[1,:,:] = [['.','.','r'],['.','r','r'],['.','r','.']]
+        self.orientations_grid[2,:,:] = [['.','.','.'],['r','r','.'],['.','r','r']]
+        self.orientations_grid[3,:,:] = [['.','r','.'],['r','r','.'],['r','.','.']]
+        self.piece_grid = self.orientations_grid[0,:,:]
 
 class S_Tetramino(Tetramino):
     """ Implementation of the S Tetramino """
-    piece_grid_rows = 3
-    piece_grid_cols = 3
-    piece_orientations = 2
+    PIECE_GRID_ROWS = 3
+    PIECE_GRID_COLS = 3
+    PIECE_ORIENTATIONS = 4
 
     def __init__(self, position=Position(0,0)):
         Tetramino.__init__(self, position)
-        # orientation 0
-        self.orientations_grid[0][0][:] = ['.','g','g']
-        self.orientations_grid[0][1][:] = ['g','g','.']
-        self.piece_grid = self.rotate(self.orientation)
+        # orientations
+        self.orientations_grid[0,:,:] = [['.','g','g'],['g','g','.'],['.','.','.']]
+        self.orientations_grid[1,:,:] = [['.','g','.'],['.','g','g'],['.','.','g']]
+        self.orientations_grid[2,:,:] = [['.','.','.'],['.','g','g'],['g','g','.']]
+        self.orientations_grid[3,:,:] = [['g','.','.'],['g','g','.'],['.','g','.']]
+        self.piece_grid = self.orientations_grid[0,:,:]
 
 class J_Tetramino(Tetramino):
     """ Implementation of the J Tetramino """
-    piece_grid_rows = 3
-    piece_grid_cols = 3
-    piece_orientations = 2
+    PIECE_GRID_ROWS = 3
+    PIECE_GRID_COLS = 3
+    PIECE_ORIENTATIONS = 4
 
     def __init__(self, position=Position(0,0)):
         Tetramino.__init__(self, position)
         # orientation 0
-        self.orientations_grid[0][0][:] = ['b','.','.']
-        self.orientations_grid[0][1][:] = ['b','b','b']
-        self.piece_grid = self.rotate(self.orientation)
+        self.orientations_grid[0,:,:] = [['b','.','.'],['b','b','b'],['.','.','.']]
+        self.orientations_grid[1,:,:] = [['.','b','b'],['.','b','.'],['.','b','.']]
+        self.orientations_grid[2,:,:] = [['.','.','.'],['b','b','b'],['.','.','b']]
+        self.orientations_grid[3,:,:] = [['.','b','.'],['.','b','.'],['b','b','.']]
+        self.piece_grid = self.orientations_grid[0,:,:]
 
 class L_Tetramino(Tetramino):
     """ Implementation of the J Tetramino """
-    piece_grid_rows = 3
-    piece_grid_cols = 3
-    piece_orientations = 2
+    PIECE_GRID_ROWS = 3
+    PIECE_GRID_COLS = 3
+    PIECE_ORIENTATIONS = 4
 
     def __init__(self, position=Position(0,0)):
         Tetramino.__init__(self, position)
         # orientation 0
-        self.orientations_grid[0][0][:] = ['.','.','o']
-        self.orientations_grid[0][1][:] = ['o','o','o']
-        self.piece_grid = self.rotate(self.orientation)
+        self.orientations_grid[0,:,:] = [['.','.','o'],['o','o','o'],['.','.','.']]
+        self.orientations_grid[1,:,:] = [['.','o','.'],['.','o','.'],['.','o','o']]
+        self.orientations_grid[2,:,:] = [['.','.','.'],['o','o','o'],['o','.','.']]
+        self.orientations_grid[3,:,:] = [['o','o','.'],['.','o','.'],['.','o','.']]
+        self.piece_grid = self.orientations_grid[0,:,:]
 
 class T_Tetramino(Tetramino):
     """ Implementation of the J Tetramino """
-    piece_grid_rows = 3
-    piece_grid_cols = 3
-    piece_orientations = 4
+    PIECE_GRID_ROWS = 3
+    PIECE_GRID_COLS = 3
+    PIECE_ORIENTATIONS = 4
 
     def __init__(self, position=Position(0,0)):
         Tetramino.__init__(self, position)
         # orientation 0
-        self.orientations_grid[0][0][:] = ['.','m','.']
-        self.orientations_grid[0][1][:] = ['m','m','m']
-        self.piece_grid = self.rotate(self.orientation)
+        self.orientations_grid[0,:,:] = [['.','m','.'],['m','m','m'],['.','.','.']]
+        self.orientations_grid[1,:,:] = [['.','m','.'],['.','m','m'],['.','m','.']]
+        self.orientations_grid[2,:,:] = [['.','.','.'],['m','m','m'],['.','m','.']]
+        self.orientations_grid[3,:,:] = [['.','m','.'],['m','m','.'],['.','m','.']]
+        self.piece_grid = self.orientations_grid[0,:,:]
 
 
 class Model(object):
@@ -137,6 +155,9 @@ class Model(object):
     def add_tetramino(self, tetramino):
         self.tetraminos.append(tetramino)
         self.active_tetramino = tetramino
+
+    def rotate_right(self):
+        self.active_tetramino.rotate_right()
 
     def test_tetramino(self):
         self.active_tetramino.test_tetramino()
@@ -196,6 +217,10 @@ class Controller(object):
                 self.model.cleared_lines += 1
                 self.model.score += 100
 
+    def print_empty_line(self):
+        print ''
+
+
     def __init__(self, model, viewer):
         self.model = model
         self.viewer = viewer
@@ -214,7 +239,9 @@ class Controller(object):
                         'S': self.model.add_tetramino,
                         'T': self.model.add_tetramino,
                         'Z': self.model.add_tetramino,
-                        't': self.model.test_tetramino}
+                        ')': self.model.rotate_right,
+                        't': self.model.test_tetramino,
+                        ';': self.print_empty_line}
 
     def exec_command(self, keystroke):
         if keystroke == 'I':
